e1a8a651e4868b7f83876f6beb12b617041455e8
mqthanggg <maiqu@THANGMAP>
2025-07-28 20:04:06 +0700

    using database as master-slave with auto promotion and recovery, mqtt handler service now in a separate dotnet project, update gitignore

---
+++ MQTTHandler/Service/Database/LogUpdateService.cs
@@ -0,0 +1,85 @@
+namespace LogUpdate{
+    public enum DispenserState{
+        IDLE,
+        PUMP,
+        RESET
+    }
+}
+public class LogUpdateService : ILogUpdateService{   
+    private class WSDispenserRecord{
+        public float liter {get; set;}
+        public int price {get; set;}
+        public DispenserState state{get; set;}
+        public int payment {get; set;}
+    }
+    private readonly IServiceScopeFactory _factory;
+    private ConcurrentDictionary<int,WSDispenserRecord> _currentData;
+    public LogUpdateService(
+        IServiceScopeFactory factory
+    ){
+        _factory = factory;
+        _currentData = new ConcurrentDictionary<int, WSDispenserRecord>();
+    }
+
+    public Task SegmentProcessAsync(int id, ReadOnlySequence<byte> segment){
+        return Task.Run(
+            async () => {
+                try
+                {
+                    WSDispenserRecord? wSDispenserRecord = JsonSerializer.Deserialize<WSDispenserRecord>(
+                        new MemoryStream(segment.ToArray())
+                    );
+                    if (wSDispenserRecord != null){
+                        await FetchDispenserData(
+                            id,
+                            wSDispenserRecord
+                        );
+                    }
+                    }
+                catch (Exception e)
+                {  
+                    Console.WriteLine($"Failed, why: {e.Message}");                    
+                }
+            }
+        );
+    }
+    private async Task FetchDispenserData(int id, WSDispenserRecord record){
+        if (
+            record.state == DispenserState.RESET &&
+            _currentData.TryGetValue(id,out var value) &&
+            value.state == DispenserState.PUMP
+        ){
+            try{
+                var scope = _factory.CreateScope();
+                var affectedRows = await scope.ServiceProvider.GetRequiredService<ILogRepository>().InsertAsync(
+                    new Log{
+                        DispenserId = id,
+                        TotalLiters = value.liter,
+                        TotalAmount = value.price,
+                        LogType = record.payment,
+                        CreatedBy = "server",
+                        LastModifiedBy = "server"
+                    }
+                );
+                if (affectedRows != 1){
+                    throw new Exception("Unable to insert to Log");
+                }
+            }
+            catch(Exception e){
+                Console.WriteLine($"Failed, why: {e.Message}");
+            }
+        }
+        else if (
+            record.state == DispenserState.PUMP
+        ){
+            lock(_currentData){
+                _currentData.AddOrUpdate(
+                    id,
+                    record,
+                    (_,_) => record
+                );
+            }
+        }
+        
+    }
+}
