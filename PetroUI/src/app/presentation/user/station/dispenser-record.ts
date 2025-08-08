264fb8017f3b3cf25cb949cfb85332187080009d
mqthanggg <maiqu@THANGMAP>
2025-08-08 09:21:48 +0700

    fix websocket problem

--- PetroUI/src/app/presentation/user/station/dispenser-record.ts
+++ PetroUI/src/app/presentation/user/station/dispenser-record.ts
@@ -11,12 +11,7 @@ export interface DispenserRecord {
 }
 
 export interface WSDispenserRecord{
-    dispenserId: number,
-    name: number,  // Number
     price: number,  
-    longName: string,
-    shortName: string,
     liter: number | undefined,
-    totalAmount: number | undefined,
-    status: string | undefined,
+    state: number | undefined,
 }
